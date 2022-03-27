f5ea3e6a4eb3a4980a7712a61659db57080f9039
shakiru shekoni <shakirushekoni@shakirus-MacBook-Pro.local>
2022-03-27 09:30:12 +0100

    brillo

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
 
   // serverMiddleware
   serverMiddleware: [
-    { path: '/api', handler: '~/api/index' }
+    { path: '/api', handler: '~/api/index.js' }
   ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
